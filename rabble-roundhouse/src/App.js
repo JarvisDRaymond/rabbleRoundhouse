0fd076e9f55364b4708db4ef521414bae16e7926
Jarvis <jarvis@jarvisdraymond.com>
2021-10-16 19:12:07 -0500

    Added functionaly for select all UI

--- rabble-roundhouse/src/App.js
+++ rabble-roundhouse/src/App.js
@@ -1,5 +1,4 @@
 import "./App.scss";
-import "./components/DataTable.js";
 import DataTable from "./components/DataTable";
 
 function App() {
--- rabble-roundhouse/src/components/DataTable.js
+++ rabble-roundhouse/src/components/DataTable.js
@@ -1,36 +1,113 @@
-import React, { useState } from 'react';
-const DataTable = ({appData}) => {
-  const [selectedCount, setSelectedCount] = useState(0);
+import React, { useState, useEffect } from "react";
+const DataTable = ({ appData }) => {
+  // SHOW DOWNLOADS FUNCTIONALITY
+  // Clicking "Download Selected" when some or all items are displayed
+  // should generate an alert box with the path and device of all selected files.
   const showDownloads = () => {
-    alert("hi");
+    let downloadItems = "";
+    appData.forEach((obj, index) => {
+      if (obj.status === "available" && checkedState[index] === true) {
+        downloadItems +=
+          "\nPath: " + obj.path + "\nDevice: " + obj.device + "\n";
+      }
+    });
+    if (downloadItems === "") {
+      alert("No items selected and available for download.");
+    } else {
+      alert("Items selected & available for download: \n" + downloadItems);
+    }
   };
-  const updateCount = (checked) => {
-      checked===true?setSelectedCount(selectedCount+1):setSelectedCount(selectedCount-1);
-  }
+
+  // Creates an array to for the storing check box inputs checked state
+  const [checkedState, setCheckedState] = useState(
+    new Array(appData.length).fill(false)
+  );
+  // Total number of check boxes available
+  const totalSelectable = checkedState.length;
+
+  // SELECT ALL FUNCTIONALITY
+  const selectAll = () => {
+    let updatedCheckedState = [];
+    let numChecked = checkedState.reduce((a, b) => a + b);
+    // Clicking the select-all checkbox should select all items if none or some are selected.
+    // Clicking the select-all checkbox should de-select all items if all are currently selected.
+    appData.forEach((obj, index) => {
+      if (numChecked < totalSelectable) {
+        updatedCheckedState.push(true);
+      } else {
+        updatedCheckedState.push(false);
+      }
+    });
+    setCheckedState(updatedCheckedState);
+  };
+
+  // Handling SelectAll State
+  const checkboxRef = React.useRef();
+  
+  useEffect(() => {
+    handleSelectAllChange();
+  });
+  const handleSelectAllChange = () => {
+    let numChecked = checkedState.reduce((a, b) => a + b);
+    // The select-all checkbox should be in an unselected state 
+    // if no items are selected.
+    if (numChecked === 0) {
+      checkboxRef.current.checked = false;
+      checkboxRef.current.indeterminate = false;
+    }
+    // The select-all checkbox should be in an indeterminate state 
+    // if some but not all items are selected.
+    else if (numChecked < totalSelectable) {
+      checkboxRef.current.checked = false;
+      checkboxRef.current.indeterminate = true;
+    } 
+    //The select-all checkbox should be in a selected state 
+    // if all items are selected.
+    else {
+      checkboxRef.current.checked = true;
+      checkboxRef.current.indeterminate = false;
+    }
+  };
+
+  // Update array storing checked states
+  const handleCheckboxChange = (position) => {
+    const updatedCheckedState = checkedState.map((item, index) =>
+      index === position ? !item : item
+    );
+    setCheckedState(updatedCheckedState);
+  };
+
+  
+  // Output JSX for Component
   return (
     <div className="dataTable">
+
+      <input name="Select All Checkbox" type="checkbox" ref={checkboxRef} onClick={selectAll} />
       <h2>
-        <input type="checkbox" />
-        {selectedCount===0?'None Selected':`Selected ${selectedCount}`}
+        {checkedState.reduce((a, b) => a + b) === 0
+          ? "None Selected"
+          : `Selected ${checkedState.reduce((a, b) => a + b)}`}
       </h2>
-      <h2 onClick={showDownloads}>&#10515; Download Selected</h2>
+      <h2 onClick={showDownloads}>&#10515;&nbsp;Download Selected</h2>
       <table>
-        <tbody>
-          <td>
+        <thead>
+          <tr>
             <th></th>
             <th>Name</th>
             <th>Device</th>
             <th>Path</th>
-          </td>
+            <th>Status</th>
+          </tr>
+        </thead>
+        <tbody>
           {appData.map((item, i) => (
-            <tr key={i}>
+            <tr key={i} className={checkedState[i] ? "selected" : ""}>
               <td>
                 <input
                   type="checkbox"
-                  onChange={(e)=>updateCount(e.currentTarget.checked)} 
-                  {...(item.status === "available"
-                    ? { disabled: false }
-                    : { disabled: true })}
+                  value={item.path}
+                  checked={checkedState[i]}
+                  onChange={() => handleCheckboxChange(i)}
                 ></input>
               </td>
               <td>{item.name}</td>
